576cfb070c0b04c4c0b5fc4ad2d63c1930986ff0
ChayaBasha <johanna.blumenthal@gmail.com>
2020-11-22 16:27:52 -0700

    work on Flight Manager

--- src/business/FlightManager.java
+++ src/business/FlightManager.java
@@ -1,5 +1,7 @@
 package business;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import domain.flights.Flight;
@@ -12,13 +14,24 @@ public class FlightManager extends ManagerSuperType {
 	public static String IFLIGHT_SVC_PROP = "IFlightSvc";
 
 	public List<Flight> searchAvailableFlights(FlightSearchCriteria customerCriteria) {
-		
+
+		LocalDateTime strictArrivalTimeRange = customerCriteria.getDesiredArrivalTime().minusHours(12);
+		LocalDateTime broadArrivalTimeRange = customerCriteria.getDesiredArrivalTime().minusDays(3);
+
 		IFlightSvc flightSvc = (IFlightSvc) getService(IFLIGHT_SVC_PROP);
-		if(customerCriteria.isDirectFlightsOnly()) {
-		flightSvc.getFlightsByRoute(customerCriteria.getDesiredDepartureCity(), customerCriteria.getDesiredArrivalCity());
-		
-	
-		
-	
-    }
+		if (customerCriteria.isDirectFlightsOnly() && customerCriteria.isPriorityTimeArrival()) {
+			List<Flight> directFlights = flightSvc.getFlightsByRoute(customerCriteria.getDesiredDepartureCity(),
+					customerCriteria.getDesiredArrivalCity());
+
+			ArrayList<Flight> matchingFlights = new ArrayList<Flight>();
+			for (int i = 0; i < directFlights.size(); i++) {
+				if (directFlights.get(i).getArrivalTime().isBefore(customerCriteria.getDesiredArrivalTime())
+						&& directFlights.get(i).getArrivalTime().isAfter(strictArrivalTimeRange)) {
+					matchingFlights.add(directFlights.get(i));
+				}
+				
+			}
+		} else if(customerCriteria.isDirectFlightsOnly())
+
+	}
 }
